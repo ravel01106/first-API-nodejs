d702848ab36002228f5e2c59ef9d820e214f8a62
ravel01106 <ravello2002@gmail.com>
2023-11-27 22:23:00 +0000

    refactor: separate the authentication logic and create the auth.http file

---
+++ auth/auth.http.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const usersController = require('./users.controller');
+
+const loginUser = (req, res) => {
+    if(!req.body){
+        return res.status(400).json({message: 'Missing data'});
+    }else if(!req.body.user || !req.body.password){
+        return res.status(400).json({message: 'Missing data'});
+    }
+
+    // Comprobamos credenciales
+    usersController.checkUserCredentials(req.body.user, req.body.password, (err, result) => {
+
+        // Si no son validas, error
+        if(err || !result){
+            return res.status(401).json({message: 'Invalid credentials.'})
+        }
+
+        // Si son validas, generamos un JWT y lo devolvemos
+        let user = usersController.getUserIdFromUserName(req.body.user);
+        const token = jwt.sign({userId: user.userId}, 'secretPassword');
+        res.status(200).json(
+            {token: token}
+        );
+    });
+}
+
+exports.loginUser = loginUser;
--- auth/auth.router.js
+++ auth/auth.router.js
@@ -1,46 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-
-// Controllers
-const usersController = require('./users.controller');
-
-router.route('/')
-    .get((req, res) => {
-        res.send('GET Auth router');
-    })
-    .post((req, res) => {
-        res.send('POST Auth router');
-    });
+const authHttpHandler = require('./auth.http')
 
 router.route('/login')
-    .post((req, res) => {
-        if(!req.body){
-            return res.status(400).json({message: 'Missing data'});
-        }else if(!req.body.user || !req.body.password){
-            return res.status(400).json({message: 'Missing data'});
-        }
-    
-        // Comprobamos credenciales
-        usersController.checkUserCredentials(req.body.user, req.body.password, (err, result) => {
-    
-            // Si no son validas, error
-            if(err || !result){
-                return res.status(401).json({message: 'Invalid credentials.'})
-            }
-    
-            // Si son validas, generamos un JWT y lo devolvemos
-            let user = usersController.getUserIdFromUserName(req.body.user);
-            const token = jwt.sign({userId: user.userId}, 'secretPassword');
-            res.status(200).json(
-                {token: token}
-            );
-            /*
-            res.status(200).json(
-                {token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.zX5MPQtbjoNAS7rpsx_hI7gqGIlXOQq758dIqyBVxxY'}
-            ); 
-            */
-        });
-    });
+    .post(authHttpHandler.loginUser);
 
 exports.router = router;
